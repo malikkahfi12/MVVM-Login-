90dd6120749bbc3ae3f96ebc51b3be930dce5f6a
malikkahfi12 <malikkahfi1996@gmail.com>
2020-04-23 11:49:56 +0700

    add kodein for dependency injection

---
+++ app/src/main/java/com/example/mvvmsampleapp/MVVMApplication.kt
@@ -0,0 +1,33 @@
+package com.example.mvvmsampleapp
+
+import android.app.Application
+import com.example.mvvmsampleapp.data.db.AppDatabase
+import com.example.mvvmsampleapp.data.network.MyApi
+import com.example.mvvmsampleapp.data.network.NetworkConnectionInterceptor
+import com.example.mvvmsampleapp.data.repository.UserRepository
+import com.example.mvvmsampleapp.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class MVVMApplication : Application(), KodeinAware {
+    override val kodein: Kodein = Kodein.lazy {
+
+//        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
+//        val api = MyApi(networkConnectionInterceptor)
+//        val db = AppDatabase(this)
+//        val repository = UserRepository(api, db)
+//        val factory = AuthViewModelFactory(repository)
+
+        import(androidXModule(this@MVVMApplication))
+        bind() from  singleton {NetworkConnectionInterceptor(instance())}
+        bind() from  singleton { MyApi(instance()) }
+        bind() from  singleton  {AppDatabase(instance()) }
+        bind() from  singleton { UserRepository(instance(), instance()) }
+        bind() from  provider { AuthViewModelFactory(instance()) }
+    }
+}
--- app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -1,37 +1,31 @@
 package com.example.mvvmsampleapp.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmsampleapp.R
-import com.example.mvvmsampleapp.data.db.AppDatabase
 import com.example.mvvmsampleapp.data.db.entity.User
-import com.example.mvvmsampleapp.data.network.MyApi
-import com.example.mvvmsampleapp.data.network.NetworkConnectionInterceptor
-import com.example.mvvmsampleapp.data.repository.UserRepository
 import com.example.mvvmsampleapp.databinding.ActivityLoginBinding
 import com.example.mvvmsampleapp.ui.home.HomeActivity
 import com.example.mvvmsampleapp.util.hide
 import com.example.mvvmsampleapp.util.show
 import com.example.mvvmsampleapp.util.snackbar
-import com.example.mvvmsampleapp.util.toast
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
+import org.kodein.di.android.kodein
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory : AuthViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = MyApi(networkConnectionInterceptor)
-        val db = AppDatabase(this)
-        val repository = UserRepository(api, db)
-        val factory = AuthViewModelFactory(repository)
+
         val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         val viewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
 
